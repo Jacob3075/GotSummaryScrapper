466778ee1fac76601e5e6a129636698339cfe796
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-06-02 10:35:28 +0530

    Completed MarkdownFormatter.

--- src/main/kotlin/com/jacob/got_summary/formatters/MarkdownFormatter.kt
+++ src/main/kotlin/com/jacob/got_summary/formatters/MarkdownFormatter.kt
@@ -6,11 +6,16 @@ class MarkdownFormatter : Formatter {
 	override val fileExtension: String
 		get() = "md"
 
-	override fun formatData(data: Chapter): String {
-		TODO("Not yet implemented")
+	override fun formatData(data: Chapter) = buildString {
+		append(title1("${data.index}. ${data.title.name}"))
+		append("\n")
+		append(body(data.content.text.joinToString("\n\n")))
 	}
 
-	override fun formatData(data: List<Chapter>): String {
-		TODO("Not yet implemented")
-	}
+	override fun formatData(data: List<Chapter>): String =
+		data.joinToString(separator = "\n\n", transform = ::formatData)
+
+	private fun title1(text: String) = """<h1 style="text-align: center;">$text</h1>"""
+	private fun title2(text: String) = """<h2 style="text-align: center;">$text</h2>"""
+	private fun body(text: String) = text
 }
