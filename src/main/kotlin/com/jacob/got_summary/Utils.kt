ad3cc7a44ffc08ba502882ffab046d5ef805e4f7
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-06-02 11:39:42 +0530

    Updated MarkdownFormatter.

--- src/main/kotlin/com/jacob/got_summary/Utils.kt
+++ src/main/kotlin/com/jacob/got_summary/Utils.kt
@@ -3,7 +3,33 @@ package com.jacob.got_summary
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
-suspend fun <T, R> Iterable<T>.mapIndexedParallel(transform: (Int, T) -> R): List<R> =
+/**
+ *  Performs map transformation on the iterable using coroutines.
+ *
+ *  [Source](https://github.com/cvb941/kotlin-parallel-operations)
+ */
+suspend inline fun <T, R> Iterable<T>.mapIndexedParallel(crossinline transform: (Int, T) -> R): List<R> =
 	coroutineScope {
 		mapIndexed { index, item -> async { transform(index, item) } }.map { it.await() }
 	}
+
+/**
+ *  Performs map transformation on the iterable using coroutines.
+ *  The chunkSize parameter is used to run multiple transformations on a single coroutine.
+ *
+ *  [Source](https://github.com/cvb941/kotlin-parallel-operations)
+ *
+ *  @param chunkSize Size of each sub-collection that will be reduced in each coroutine.
+ */
+suspend inline fun <T, R> Iterable<T>.mapIndexedParallelChunked(
+	chunkSize: Int,
+	crossinline transform: suspend (index: Int, T) -> R,
+): List<R> = coroutineScope {
+	withIndex().chunked(chunkSize)
+			.map { subChunk ->
+				async {
+					subChunk.map { transform(it.index, it.value) }
+				}
+			}
+			.flatMap { it.await() }
+}
--- src/main/kotlin/com/jacob/got_summary/formatters/MarkdownFormatter.kt
+++ src/main/kotlin/com/jacob/got_summary/formatters/MarkdownFormatter.kt
@@ -9,13 +9,18 @@ class MarkdownFormatter : Formatter {
 	override fun formatData(data: Chapter) = buildString {
 		append(title1("${data.index}. ${data.title.name}"))
 		append("\n")
-		append(body(data.content.text.joinToString("\n\n")))
+		append(body(data.content.text.joinToString("\n")))
+		append(divider())
 	}
 
 	override fun formatData(data: List<Chapter>): String =
-		data.joinToString(separator = "\n\n", transform = ::formatData)
+		data.joinToString(separator = "", transform = ::formatData)
 
 	private fun title1(text: String) = """<h1 style="text-align: center;">$text</h1>"""
+
 	private fun title2(text: String) = """<h2 style="text-align: center;">$text</h2>"""
+
 	private fun body(text: String) = text
+
+	private fun divider() = "\n\n---\n\n"
 }
